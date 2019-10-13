76b88cc42145eec0acc2621573d11b7e3c41cc7f
Luca Marchesano <lucamarche-iz1mlt@users.noreply.github.com>
2019-10-13 22:01:17 +0200

    Update from the master

--- Config.h
+++ Config.h
@@ -44,11 +44,13 @@
 // For the original Arduino Due pin layout
 // #define ARDUINO_DUE_PAPA
 
-// For the ZUM V1.0 and V1.0.1 boards pin layout
-// #define ARDUINO_DUE_ZUM_V10
-
+#if defined(STM32F1)
 // For the SQ6POG board
-// #define STM32F1_POG
+#define STM32F1_POG
+#else
+// For the ZUM V1.0 and V1.0.1 boards pin layout
+//#define ARDUINO_DUE_ZUM_V10
+#endif
 
 // For the SP8NTH board
 // #define ARDUINO_DUE_NTH
